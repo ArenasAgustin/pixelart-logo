c6962add0805613f1781d61376529587d1c72a94
“ArenasAgustin” <arenasagustin7@gmail.com>
2021-12-27 18:46:17 -0300

    background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Title from "./components/Title/Title";
 import Pixelart from "./components/Pixelart/Pixelart";
+import Background from "./components/Background/Background";
 import "./App.scss";
 
 function App() {
   return (
-    <div className="app">
-      <Pixelart />
-      <Title />
+    <div className="body">
+      <div className="app">
+        <Pixelart />
+        <Title />
+      </div>
+
+      <Background />
     </div>
   );
 }
---
+++ src/components/Background/Background.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Background.scss";
+
+export default function Background() {
+  return (
+    <div class="background">
+      <div class="polygon"></div>
+      <div class="triangle triangle-1"></div>
+      <div class="triangle triangle-2"></div>
+      <div class="triangle triangle-3"></div>
+      <div class="triangle triangle-4"></div>
+    </div>
+  );
+}
